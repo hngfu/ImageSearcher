72a0c7e4387d5c9ea91244a02e00d1f6acfcb958
hngfu <djsl1018@naver.com>
2021-05-22 23:16:59 +0900

    Implement ImageSearchViewModel

--- ImageSearcher/ImageSearcher/Features/ImageSearch/ImageSearchViewModel.swift
+++ ImageSearcher/ImageSearcher/Features/ImageSearch/ImageSearchViewModel.swift
@@ -5,8 +5,39 @@
 //  Created by 조재흥 on 2021/05/21.
 //
 
-import Foundation
+import RxRelay
 
 final class ImageSearchViewModel {
     
+    let searchedImageInfoRelay = PublishRelay<[SearchedImageInfo]>()
+    
+    func search(word: String) {
+        imageSearchManager.search(word: word) { [weak self] searchedResults in
+            guard let `self` = self else { return }
+            self.nextPageFetchable = (searchedResults.meta.isEnd == false)
+            self.searchedImageInfo = searchedResults.documents
+            self.searchedImageInfoRelay.accept(self.searchedImageInfo)
+        } failureHandler: { error in
+            //TODO: add failureHandler
+            print(error)
+        }
+    }
+    
+    func fetchNextPage() {
+        guard nextPageFetchable else { return }
+        imageSearchManager.fetchNextPage { [weak self] nextPage in
+            guard let `self` = self else { return }
+            self.nextPageFetchable = (nextPage.meta.isEnd) == false
+            self.searchedImageInfo += nextPage.documents
+            self.searchedImageInfoRelay.accept(self.searchedImageInfo)
+        } failureHandler: { error in
+            //TODO: add failureHandler
+            print(error)
+        }
+    }
+    
+    //MARK: - Private
+    private let imageSearchManager = ImageSearchManager()
+    private var searchedImageInfo = [SearchedImageInfo]()
+    private var nextPageFetchable = Bool()
 }
