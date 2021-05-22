ade6b3d1c93a751f31a6773e9d4f7164e229230a
hngfu <djsl1018@naver.com>
2021-05-22 23:46:43 +0900

    Add UICollectionViewDelegateFlowLayout to ImageSearchViewController
    채택, 준수

--- ImageSearcher/ImageSearcher/Features/ImageSearch/ImageSearchViewController.swift
+++ ImageSearcher/ImageSearcher/Features/ImageSearch/ImageSearchViewController.swift
@@ -18,6 +18,8 @@ final class ImageSearchViewController: UIViewController, Storyboarded, ViewModel
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchedImageCollectionView.rx.setDelegate(self)
+            .disposed(by: disposeBag)
         bind()
     }
     
@@ -64,4 +66,20 @@ final class ImageSearchViewController: UIViewController, Storyboarded, ViewModel
     //MARK: - Private
     private var disposeBag = DisposeBag()
     private let imageManager = ImageManager()
+}
+
+extension ImageSearchViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let sideLength = UIScreen.main.bounds.width / 3
+        return .init(width: sideLength, height: sideLength)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
