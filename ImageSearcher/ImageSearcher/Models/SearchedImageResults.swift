9bb4210fa12a5e605dddb9166f277e6d024dcbfc
hngfu <djsl1018@naver.com>
2021-05-23 17:48:21 +0900

    Add width, height to SearchedImageInfo

--- ImageSearcher/ImageSearcher/Models/SearchedImageResults.swift
+++ ImageSearcher/ImageSearcher/Models/SearchedImageResults.swift
@@ -27,11 +27,15 @@ struct SearchedImageInfo: Decodable {
     let imageURL: String
     let siteName: String
     let dateTime: String
+    let width: Int
+    let height: Int
     
     enum CodingKeys: String, CodingKey {
         case thumbnailURL = "thumbnail_url"
         case imageURL = "image_url"
         case siteName = "display_sitename"
         case dateTime = "datetime"
+        case width = "width"
+        case height = "height"
     }
 }
