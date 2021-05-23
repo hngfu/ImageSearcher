6e3438b648263aadb20ad8d2c681e8fb4bed564f
hngfu <djsl1018@naver.com>
2021-05-23 23:18:03 +0900

    Implement DetailImageViewController

--- ImageSearcher/ImageSearcher/Features/DetailImage/DetailImageViewController.swift
+++ ImageSearcher/ImageSearcher/Features/DetailImage/DetailImageViewController.swift
@@ -5,18 +5,47 @@
 //  Created by 조재흥 on 2021/05/23.
 //
 
-import UIKit
+import RxSwift
 
 final class DetailImageViewController: UIViewController, Storyboarded, ViewModelBindable {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var siteNameLabel: UILabel!
+    @IBOutlet weak var dateTimeLabel: UILabel!
+    @IBOutlet weak var imageViewHeightConstraint: NSLayoutConstraint!
+    
     var viewModel: DetailImageViewModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bind()
+    }
 
     }
     
     func bind() {
-        
+        viewModel?.searchedImageInfoRelay.asDriver()
+            .drive(onNext: { [weak self] info in
+                guard let `self` = self else { return }
+                self.siteNameLabel.isHidden = (info.siteName.count == 0) ? true : false
+                self.siteNameLabel.text = "출처: \(info.siteName)"
+                
+                self.dateTimeLabel.isHidden = (info.dateTime.count == 0) ? true : false
+                self.dateTimeLabel.text = "작성 시간: \(info.dateTime)"
+                
+                let height = (Int(self.imageView.bounds.width) * info.height) / info.width
+                self.imageViewHeightConstraint.constant = CGFloat(height)
+                
+                self.imageManager.image(urlString: info.imageURL) { (_, image) in
+                    DispatchQueue.main.async {
+                        self.imageView.image = image
+                    }
+                }
+            })
+            .disposed(by: disposeBag)
     }
+    
+    //MARK: - Private
+    private let disposeBag = DisposeBag()
+    private let imageManager = ImageManager()
 }
--- ImageSearcher/ImageSearcher/Features/DetailImage/DetailImageViewModel.swift
+++ ImageSearcher/ImageSearcher/Features/DetailImage/DetailImageViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by 조재흥 on 2021/05/23.
 //
 
-import Foundation
+import RxRelay
 
 protocol DetailImageViewModelDelegate: AnyObject {
     
@@ -15,7 +15,10 @@ final class DetailImageViewModel {
     
     weak var delegate: DetailImageViewModelDelegate?
     
+    let searchedImageInfoRelay: BehaviorRelay<SearchedImageInfo>
+    
+    //MARK: - Init
     init(info: SearchedImageInfo) {
-        
+        searchedImageInfoRelay = BehaviorRelay<SearchedImageInfo>(value: info)
     }
 }
